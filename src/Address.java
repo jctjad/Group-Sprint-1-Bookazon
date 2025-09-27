7f05f3e1f3e8ec5eb3fe114b5b122d72b06ecd01
Alex Solano <alexsolano@Alexs-MacBook-Air-6.local>
2025-09-27 17:18:30 -0400

    created Address class with appropriate fields

---
+++ src/Address.java
@@ -0,0 +1,9 @@
+public class Address {
+    private String addressLine1;
+    private String adressLine2;
+    private String addressCity;
+    private String addressState;
+    private String addressZip;
+    private String addressCountry;
+    
+}
