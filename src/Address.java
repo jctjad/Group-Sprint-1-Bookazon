c89c0f2a80e286387f9eaf18f763cd419f06ceb6
Alex Solano <alexsolano@Alexs-MacBook-Air-6.local>
2025-09-27 17:20:32 -0400

    added setAddress() to Address class

--- src/Address.java
+++ src/Address.java
@@ -1,9 +1,18 @@
 public class Address {
     private String addressLine1;
-    private String adressLine2;
+    private String addressLine2;
     private String addressCity;
     private String addressState;
     private String addressZip;
     private String addressCountry;
+
+    public void setAddress(String line1, String line2, String city, String state, String zip, String country){
+        this.addressLine1 = line1;
+        this.addressLine2 = line2;
+        this.addressCity = city;
+        this.addressState = state;
+        this.addressZip = zip;
+        this.addressCountry = country;
+    }
     
 }
