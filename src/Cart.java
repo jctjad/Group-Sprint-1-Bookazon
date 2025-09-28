ea14a76115e4554fcaa20a0becaa179daf6dfe00
Samuel Atilano <samuel4atilano@icloud.com>
2025-09-28 07:12:34 -0400

    Fixed Issue-3 in updateQuantity and viewCartDetails

--- src/Cart.java
+++ src/Cart.java
@@ -15,23 +15,26 @@ public class Cart {
         items.remove(item);
     }
     
+    //Issue-3: Removing search for item with index method
     public void updateQuantity(CartItem item, int quantity) {
-        for (CartItem cartItem : items) {
-            if (cartItem.equals(item)) {
-                cartItem.setQuantity(quantity);
-                break;
-            }
-        }
+        int index = items.indexOf(item);
+        items.get(index).setQuantity(quantity);
     }
     
+    //Issue-3: Created helper function
     public void viewCartDetails() {
         System.out.println("Cart Details:");
-        for (CartItem item : items) {
-            System.out.println(item.getName() + " - Quantity: " + item.getQuantity());
+        for (CartItem item: items){
+            itemDetails(item);
         }
         System.out.println("\n");
     }
     
+    public void itemDetails(CartItem item){
+        System.out.println(item.getName() + " - Quantity: " + item.getQuantity());
+    } 
+
+
     public ArrayList<CartItem> getItems() {
         return items;
     }
