16f4f42a39064eda17f6ec44a070858c25a6ad5a
jctjad <122404431+jctjad@users.noreply.github.com>
2025-09-28 21:13:49 -0400

    Created abstract Media class

---
+++ src/Media.java
@@ -0,0 +1,54 @@
+public abstract class Media {
+    private String title;
+    private String author;
+    private int yearPublished;
+    private double price;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getYearPublished() {
+        return yearPublished;
+    }
+
+    public void setYearPublished(int yearPublished) {
+        this.yearPublished = yearPublished;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isPriceValid() {
+        return price > 0;
+    }
+
+    public boolean isTitleValid() {
+        return title != null && !title.isEmpty();
+    }
+
+    public boolean isAuthorValid() {
+        return author != null && !author.isEmpty();
+    }
+
+    public boolean isYearPublishedValid() {
+        return yearPublished > 0;
+    }
+}
